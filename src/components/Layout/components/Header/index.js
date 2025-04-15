ea1e5acf5d96246ff478c8dad08b306a10e0a680
Tien Dat <dotiend3@gmail.com>
2025-04-15 17:05:52 +0700

    LayoutHeader UI 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,16 +1,34 @@
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 import styles from "./Header.module.scss";
 import classNames from "classnames/bind";
+import images from "~/assets/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const cx = classNames.bind(styles);
 function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        {/*Logo*/}
-        {/*Search*/}
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+        <div className={cx("search")}>
+          <input placeholder="Tìm kiếm " spellCheck={false} />
+          <button className={cx("clear-btn")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx("action")}></div>
       </div>
     </header>
   );
 }
-
 export default Header;
